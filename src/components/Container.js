4ad2e334bf379ccfae84f0ab7478a5e501e00c2d
Albert Cervantes <cervanalbert@gmail.com>
2020-05-07 01:25:18 -0700

    changed to simple container, row, and col

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Container(props) {
-  return (
-    <div className={`container${props.fluid ? "-fluid" : ""}`}>
-      {props.children}
-    </div>
-  );
+    return (
+        <div className={`container`}>
+            {props.children}
+        </div>
+    )
 }
 
 export default Container;
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Row(props) {
-  return (
-    <div className={`row${props.fluid ? "-fluid" : ""}`}>{props.children}</div>
-  );
+    return (
+        <div className={`row`}>
+            {props.children}
+        </div>
+    )
 }
 
 export default Row;
