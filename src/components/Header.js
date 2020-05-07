4d54e5e13cc5acca77a9891de446bbab2f131010
Albert Cervantes <cervanalbert@gmail.com>
2020-05-07 03:35:45 -0700

    added animate to text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
 
 function Header() {
-    // const style = {fontWeight:900}
     return(
         <header className="w3-container w3-red w3-padding" id="myHeader">
             <div className="w3-center">
-                <h2>Welcome to my First React App!</h2>
+                <h2 className="w3-animate-bottom">Welcome to my First React App!</h2>
                 <h1 className="w3-xxxlarge w3-animate-bottom">User Directory</h1>
                 <div className="w3-padding-32"></div>
             </div>
