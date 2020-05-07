8b5f8ba85096ef2e4f11c84b00fc833b4cc4a3da
Albert Cervantes <cervanalbert@gmail.com>
2020-05-07 02:45:37 -0700

    added w3 row

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Row(props) {
     return (
-        <div className={`row`}>
+        <div className={`w3-row`}>
             {props.children}
         </div>
     )
