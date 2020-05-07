0d4c89c480938fee62bc338cb0b7c0061db3677d
Albert Cervantes <cervanalbert@gmail.com>
2020-05-07 02:46:33 -0700

    added w3 container

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Container(props) {
     return (
-        <div className={`container`}>
+        <div className={`w3-container`}>
             {props.children}
         </div>
     )
