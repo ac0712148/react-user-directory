97a145663f91be9d390d2dccb563f0e34ee22e15
Albert Cervantes <cervanalbert@gmail.com>
2020-05-07 20:24:22 -0700

    generating 10 users with rowData

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,16 +1,21 @@
 import React from "react";
+import RowData from "./RowData";
 
 function Table() {
+    const users = [];
+    for(let i = 0; i < 10; i++) {
+        users.push(<RowData key={i}/>)
+    }
     return (
         <div>
             <hr />
-            <div class = "w3-center">
+            <div className = "w3-center">
                 <h2> Search Bar Goes Here! </h2>
-                <p class = "w3-large" > Search by Name, Username, Email, or Phone to find user.</p>
-                <div class = "w3-responsive w3-card-4">
-                    <table class = "w3-table w3-bordered w3-centered">
+                <p className = "w3-large" > Search by Name, Username, Email, or Phone to find user.</p>
+                <div className = "w3-responsive w3-card-4">
+                    <table className = "w3-table w3-bordered w3-centered">
                         <thead>
-                            <tr class = "w3-black">
+                            <tr className = "w3-black">
                                 <th > Name </th>
                                 <th > User </th>
                                 <th > Email </th>
@@ -18,18 +23,7 @@ function Table() {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td> John Doe </td>
-                                <td> jDoe </td>
-                                <td> test@test.com </td>
-                                <td> 123-456-7890 </td>
-                            </tr>
-                            <tr>
-                                <td> Jane Doe </td>
-                                <td> jDoe2 </td>
-                                <td> test2@test.com </td>
-                                <td> 123-456-7890 </td>
-                            </tr>
+                            {users}
                         </tbody>
                     </table>
                 </div>
