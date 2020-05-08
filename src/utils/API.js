771b79c1be8374ff689e4d1e4510cf09b3e44314
Albert Cervantes <cervanalbert@gmail.com>
2020-05-07 20:23:20 -0700

    added axios call to api

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-const BASEURL = "https://www.omdbapi.com/?t=";
-const APIKEY = "&apikey=trilogy";
 
 export default {
-  search: function (query) {
-    return axios.get(BASEURL + query + APIKEY);
+  getUser: function () {
+    return axios.get(
+      `https://randomuser.me/api/`
+    );
   },
 };
