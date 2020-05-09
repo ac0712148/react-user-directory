3397edc62163fd5b4dad02c03ce040e57916f0ae
Albert Cervantes <cervanalbert@gmail.com>
2020-05-09 00:10:10 -0700

    changed user container from function to class

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -1,23 +1,30 @@
 import React, { Component } from "react";
 import Header from "./Header";
-import Container from "./Container";
-import Row from "./Row";
-import Col from "./Col";
-import Table from "./Table";
-
+import API from "../utils/API"
+import Table from "../components/Table"
 
 class UserContainer extends Component {
+    state = {
+        users: [],
+        search: "",
+        filteredUsers: [],
+    };
+
+    componentDidMount(){
+        this.loadUsers()
+    }
+    loadUsers = () => {
+        API.getUser()
+        .then((res) => {
+            this.setState({users: res.data.results})
+        })
+    }
     render() {
         return (
             <div>
                 <Header />
-                <Container>
-                    <Row>
-                        <Col> 
-                            <Table /> 
-                        </Col>
-                    </Row>
-                </Container>
+                <hr />
+                <Table users={this.state.users}/>
             </div>
         )
     }
