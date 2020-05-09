12248e073f0d4badc8013751e05b7cbf4478c2fa
Albert Cervantes <cervanalbert@gmail.com>
2020-05-09 00:08:03 -0700

    format edit

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
   getUser: function () {
     return axios.get(
-      `https://randomuser.me/api/`
+      `https://randomuser.me/api/?results=5`
     );
   },
 };
