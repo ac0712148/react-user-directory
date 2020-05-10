9a3a6aa6d5e8199bdc4c1022e68a06f2a5ca3f54
Albert Cervantes <cervanalbert@gmail.com>
2020-05-09 20:33:11 -0700

    removed comments

--- src/components/Table.js
+++ src/components/Table.js
@@ -46,39 +46,4 @@ function Table(props) {
     )
 }
 
-// function Table() {
-//     const users = [];
-//     for(let i = 0; i < 5; i++) {
-//         users.push(<RowData key={i}/>)
-//     }
-
-//     console.log("Final: " + finalinput);
-//     return (
-//         <div>
-//             <hr />
-//             <div className = "w3-center">
-//                 <SearchForm userData={data} />
-//                 <div className="w3-padding-small"></div>
-//                 <div className = "w3-responsive w3-card-4">
-//                     <table className = "w3-table w3-bordered w3-centered">
-//                         <thead>
-//                             <tr className = "w3-black">
-//                                 <th > Name </th>
-//                                 <th > User </th>
-//                                 <th > Email </th>
-//                                 <th > Phone </th>
-//                             </tr>
-//                         </thead>
-//                         <tbody>
-//                             {users}
-//                         </tbody>
-//                     </table>
-//                 </div>
-//                 <div className="w3-padding-32"></div>
-//             </div>
-//         </div>
-        
-//     )
-// }
-
 export default Table
