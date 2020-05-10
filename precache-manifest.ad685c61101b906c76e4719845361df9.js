dc49c66467b13127a91fa1d018d0ab7cfd13eb2c
Albert Cervantes <cervanalbert@gmail.com>
2020-05-09 18:39:45 -0700

    Updates

---
+++ precache-manifest.ad685c61101b906c76e4719845361df9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740151f76c1a6bb0c0f21d8abe5bc232",
+    "url": "/react-user-directory/index.html"
+  },
+  {
+    "revision": "9589271824b462bf2ee8",
+    "url": "/react-user-directory/static/css/2.0c363312.chunk.css"
+  },
+  {
+    "revision": "9589271824b462bf2ee8",
+    "url": "/react-user-directory/static/js/2.a6b55a7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-user-directory/static/js/2.a6b55a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b2814066a66f7c9c8e",
+    "url": "/react-user-directory/static/js/main.bb3e7206.chunk.js"
+  },
+  {
+    "revision": "d1e31fde1bfe3a1c805d",
+    "url": "/react-user-directory/static/js/runtime-main.8dfbec49.js"
+  }
+]);
